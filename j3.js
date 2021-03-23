07ab97ce988fcc5b60a3f5e55350a75965ec9396
Keikishim2 <67738474+Keikishim2@users.noreply.github.com>
2021-03-23 14:15:42 +0800

    Add files via upload

---
+++ j3.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".img1").hover(function() {
+        $( '.img1' ).attr("src","art.gif");
+        }, function() {
+        $( '.img1' ).attr("src","art1.gif");
+    })
+    $(".img2").hover(function() {
+        $( '.img2' ).attr("src","art.gif");
+        }, function() {
+        $( '.img2' ).attr("src","art1.gif");
+    })
+    $(".img3").hover(function() {
+        $( '.img3' ).attr("src","art.gif");
+        }, function() {
+        $( '.img3' ).attr("src","art1.gif");
+    })
+    $(".img4").hover(function() {
+        $( '.img4' ).attr("src","art.gif");
+        }, function() {
+        $( '.img4' ).attr("src","art1.gif");
+    })
+});
